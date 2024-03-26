c80711664d4dc61a802d565ca537396f40efd205
Mazinda India <149708409+mazinda-india@users.noreply.github.com>
2024-03-26 22:49:29 +0530

    initial commit

---
+++ screens/auth/Login.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const Login = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full `}>
+      <Text>Login</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
