26e88325853d0893e040212f46e639f1df39ca54
abhey-gupta <abheygupta1731@gmail.com>
2024-03-26 22:55:01 +0530

    initial commit

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView } from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 const Home = () => {
